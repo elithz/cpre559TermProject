9fc677c2d7218610301ca30d1a19cc6c7d558cd3
狮子劫博丽 <elithzhang@gmail.com>
2019-04-16 02:58:43 -0500

    Create testWindow.java

---
+++ src/main/java/com/amazonaws/samples/testWindow.java
@@ -0,0 +1,67 @@
+package com.amazonaws.samples;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class testWindow {
+
+	private JFrame frmHi;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					testWindow window = new testWindow();
+					window.frmHi.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public testWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmHi = new JFrame();
+		frmHi.setTitle("Hi");
+		frmHi.setBounds(100, 100, 450, 300);
+		frmHi.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		frmHi.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JButton btnNewButton = new JButton("click me!");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JFrame aframe = new JFrame();
+				aframe.setVisible(true);
+				aframe.setName("a test window");
+				aframe.setTitle("Hi");
+				aframe.setBounds(100, 100, 450, 300);
+				aframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			}
+		});
+		btnNewButton.setBounds(10, 10, 93, 23);
+		panel.add(btnNewButton);
+	}
+}
