197bcfdabf0620361bfc014f6db193bb3b5b2fee
狮子劫博丽 <elithzhang@gmail.com>
2019-04-20 15:21:58 -0500

    Update InitialWindow.java
    添加让用户选择区域

--- src/main/java/com/amazonaws/samples/InitialWindow.java
+++ src/main/java/com/amazonaws/samples/InitialWindow.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
@@ -18,8 +19,11 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 
 import java.awt.Font;
+import javax.swing.JList;
+import java.awt.Choice;
 
 public class InitialWindow {
 
@@ -68,23 +72,43 @@ public class InitialWindow {
 		panel.setLayout(null);
 
 		textField = new JTextField();
-		textField.setBounds(182, 135, 66, 21);
+		textField.setBounds(182, 82, 66, 21);
 		panel.add(textField);
 		textField.setColumns(10);
 
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(182, 80, 66, 21);
+		textField_1.setBounds(182, 51, 66, 21);
 		panel.add(textField_1);
+		
+		Choice choice = new Choice();
+		choice.setBounds(182, 124, 66, 21);
+		choice.addItem("us-east-1");
+		choice.addItem("us-east-2");
+		choice.addItem("us-west-1");
+		choice.addItem("us-west-2");
+		panel.add(choice);
 
 		JButton btnNewButton = new JButton("Log in");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println(textField.getText() + "   " + textField_1.getText());
-				user_credential = new BasicAWSCredentials(textField.getText(), textField_1.getText());
+				System.out.println(textField_1.getText() + "   " + textField.getText());
+				user_credential = new BasicAWSCredentials(textField_1.getText(), textField.getText());
 				try {
-					s3Client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2)
+					s3Client = AmazonS3ClientBuilder.standard().withRegion(choice.getSelectedItem())
 							.withCredentials(new AWSStaticCredentialsProvider(user_credential)).build();
+					try {
+						List<Bucket> buckets = s3Client.listBuckets();
+						System.out.println(buckets.get(1).getName());
+					} catch (Exception e) {
+						JFrame aframe = new JFrame();
+						aframe.setVisible(true);
+						aframe.setName("a test window");
+						aframe.setTitle("Hi");
+						aframe.setBounds(100, 100, 450, 300);
+						aframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+						throw new AmazonClientException("Credential is not valid, please check with your admin.", e);
+					}
 				} catch (Exception e) {
 					throw new AmazonClientException("Credential is not valid, please check with your admin.", e);
 				}
@@ -100,12 +124,15 @@ public class InitialWindow {
 		panel.add(lblWelcomeToAws);
 
 		JLabel lblIamAccessKey = new JLabel("IAM Access Key");
-		lblIamAccessKey.setBounds(47, 83, 84, 15);
+		lblIamAccessKey.setBounds(40, 54, 84, 15);
 		panel.add(lblIamAccessKey);
 
 		JLabel lblIamSecretAccess = new JLabel("IAM Secret Access Key");
-		lblIamSecretAccess.setBounds(26, 138, 146, 15);
+		lblIamSecretAccess.setBounds(26, 85, 146, 15);
 		panel.add(lblIamSecretAccess);
+		
+		JLabel lblRegion = new JLabel("Region");
+		lblRegion.setBounds(54, 124, 54, 15);
+		panel.add(lblRegion);
 	}
-
 }
