6adf9e7aa0a89b503da2841211cbee651ab1c544
狮子劫博丽 <elithzhang@gmail.com>
2019-04-21 03:32:29 -0500

    Update S3Console.java
    添加上传和下载文件功能

--- src/main/java/com/amazonaws/samples/S3Console.java
+++ src/main/java/com/amazonaws/samples/S3Console.java
@@ -3,6 +3,10 @@ package com.amazonaws.samples;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
@@ -13,12 +17,16 @@ import javax.swing.JOptionPane;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.mediastoredata.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.ListVersionsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.amazonaws.services.s3.model.S3VersionSummary;
 import com.amazonaws.services.s3.model.VersionListing;
@@ -26,6 +34,7 @@ import com.amazonaws.services.s3.model.VersionListing;
 import javax.swing.JLabel;
 import javax.swing.ListModel;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 
 public class S3Console {
 
@@ -191,6 +200,92 @@ public class S3Console {
 		});
 		btnDelete.setBounds(102, 228, 82, 23);
 		frame.getContentPane().add(btnDelete);
+
+		JButton btnUpload = new JButton("Upload");
+		btnUpload.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String target_bucket = list.getSelectedValue();
+				System.out.println(target_bucket);
+				JFileChooser chooser = new JFileChooser();
+				File selectedFile = null;
+				if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+					selectedFile = chooser.getSelectedFile();
+					try {
+						System.out.println("Uploading" + selectedFile.getName() + "\n");
+						InitialWindow.s3Client.putObject(target_bucket, selectedFile.getName(), selectedFile);
+						System.out.println("Download finished");
+					} catch (AmazonServiceException ase) {
+						System.out.println("Caught an AmazonServiceException, which means your request made it "
+								+ "to Amazon S3, but was rejected with an error response for some reason.");
+						System.out.println("Error Message:    " + ase.getMessage());
+						System.out.println("HTTP Status Code: " + ase.getStatusCode());
+						System.out.println("AWS Error Code:   " + ase.getErrorCode());
+						System.out.println("Error Type:       " + ase.getErrorType());
+						System.out.println("Request ID:       " + ase.getRequestId());
+					} catch (AmazonClientException ace) {
+						System.out.println("Caught an AmazonClientException, which means the client encountered "
+								+ "a serious internal problem while trying to communicate with S3, "
+								+ "such as not being able to access the network.");
+						System.out.println("Error Message: " + ace.getMessage());
+					}
+					ListObjectsV2Result result = InitialWindow.s3Client.listObjectsV2(target_bucket);
+					List<S3ObjectSummary> objects = result.getObjectSummaries();
+					model_1.clear();
+					for (int i = 0; i < objects.size(); i++) {
+						model_1.addElement(objects.get(i).getKey());
+					}
+				} else
+					System.out.println("user canceled upload");
+			}
+		});
+		btnUpload.setBounds(328, 49, 93, 23);
+		frame.getContentPane().add(btnUpload);
+
+		JButton button = new JButton("Download");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String target_file = list_1.getSelectedValue();
+				String target_bucket = list.getSelectedValue();
+				String save_path = null;
+				JFileChooser chooser = new JFileChooser();
+				chooser.setCurrentDirectory(new java.io.File("."));
+				chooser.setDialogTitle("Choose the directory you want to save the file");
+				chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				chooser.setAcceptAllFileFilterUsed(false);
+				if (chooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+					save_path = chooser.getSelectedFile().getAbsolutePath();
+					System.out.println(chooser.getSelectedFile().getAbsolutePath());
+					System.out.println(chooser.getSelectedFile());
+					System.out.println(chooser.getCurrentDirectory());
+					try {
+						S3Object o = InitialWindow.s3Client.getObject(target_bucket, target_file);
+						S3ObjectInputStream s3is = o.getObjectContent();
+						FileOutputStream fos = new FileOutputStream(new File(save_path + File.separator + target_file));
+						byte[] read_buf = new byte[1024];
+						int read_len = 0;
+						while ((read_len = s3is.read(read_buf)) > 0) {
+							fos.write(read_buf, 0, read_len);
+						}
+						System.out.println("Download finished");
+						s3is.close();
+						fos.close();
+					} catch (AmazonServiceException e1) {
+						System.err.println(e1.getErrorMessage());
+						System.exit(1);
+					} catch (FileNotFoundException e1) {
+						System.err.println(e1.getMessage());
+						System.exit(1);
+					} catch (IOException e1) {
+						System.err.println(e1.getMessage());
+						System.exit(1);
+					}
+				} else
+					System.out.println("user canceled download");
+
+			}
+		});
+		button.setBounds(328, 97, 93, 23);
+		frame.getContentPane().add(button);
 
 	}
 
