63436b6e51acbe4a2ffba93cb6ed7757f58bab89
狮子劫博丽 <elithzhang@gmail.com>
2019-04-20 23:43:19 -0500

    Update S3Console.java
    添加桶和删除桶完成

--- src/main/java/com/amazonaws/samples/S3Console.java
+++ src/main/java/com/amazonaws/samples/S3Console.java
@@ -3,20 +3,29 @@ package com.amazonaws.samples;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.ListVersionsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.S3VersionSummary;
+import com.amazonaws.services.s3.model.VersionListing;
 
 import javax.swing.JLabel;
 import javax.swing.ListModel;
+import javax.swing.JButton;
 
 public class S3Console {
 
@@ -58,50 +67,127 @@ public class S3Console {
 
 		model = new DefaultListModel<>();
 		model_1 = new DefaultListModel<>();
-		
+
 		JLabel lblBuckets = new JLabel("Buckets");
 		lblBuckets.setBounds(10, 27, 54, 15);
 		frame.getContentPane().add(lblBuckets);
-		
-		
-		
+
 		JLabel lblItems = new JLabel("Item(s)");
-		lblItems.setBounds(156, 27, 54, 15);
+		lblItems.setBounds(198, 27, 54, 15);
 		frame.getContentPane().add(lblItems);
-		
+
 		JList<String> list_1 = new JList<>(model_1);
-		list_1.setBounds(156, 52, 120, 199);
+		list_1.setBounds(198, 52, 120, 199);
 		frame.getContentPane().add(list_1);
-		
+
 		JList<String> list = new JList<>(model);
-		list.setBounds(10, 52, 120, 199);
+		list.setBounds(10, 52, 178, 166);
 		frame.getContentPane().add(list);
+
+		JButton btnNewButton = new JButton("Add");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = JOptionPane.showInputDialog("What's the name of the new bucket?");
+				try {
+					InitialWindow.s3Client.createBucket(name);
+					List<Bucket> buckets = InitialWindow.s3Client.listBuckets();
+					model.clear();
+					for (int i = 0; i < buckets.size(); i++) {
+						model.addElement(buckets.get(i).getName());
+					}
+				} catch (AmazonS3Exception e1) {
+					System.err.println(e1.getErrorMessage());
+				}
+			}
+		});
+		btnNewButton.setBounds(10, 228, 82, 23);
+		frame.getContentPane().add(btnNewButton);
 		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name_to_remove = list.getSelectedValue();
+				System.out.println(name_to_remove);
+				
+				try {
+				    System.out.println(" - removing objects from bucket");
+				    ObjectListing object_listing = InitialWindow.s3Client.listObjects(name_to_remove);
+				    while (true) {
+				        for (Iterator<?> iterator =
+				                object_listing.getObjectSummaries().iterator();
+				                iterator.hasNext();) {
+				            S3ObjectSummary summary = (S3ObjectSummary)iterator.next();
+				            InitialWindow.s3Client.deleteObject(name_to_remove, summary.getKey());
+				        }
+
+				        // more object_listing to retrieve?
+				        if (object_listing.isTruncated()) {
+				            object_listing = InitialWindow.s3Client.listNextBatchOfObjects(object_listing);
+				        } else {
+				            break;
+				        }
+				    };
+
+				    System.out.println(" - removing versions from bucket");
+				    VersionListing version_listing = InitialWindow.s3Client.listVersions(
+				            new ListVersionsRequest().withBucketName(name_to_remove));
+				    while (true) {
+				        for (Iterator<?> iterator =
+				                version_listing.getVersionSummaries().iterator();
+				                iterator.hasNext();) {
+				            S3VersionSummary vs = (S3VersionSummary)iterator.next();
+				            InitialWindow.s3Client.deleteVersion(
+				            		name_to_remove, vs.getKey(), vs.getVersionId());
+				        }
+
+				        if (version_listing.isTruncated()) {
+				            version_listing = InitialWindow.s3Client.listNextBatchOfVersions(
+				                    version_listing);
+				        } else {
+				            break;
+				        }
+				    }
+				} catch (AmazonServiceException e1) {
+				    System.err.println(e1.getErrorMessage());
+				    System.exit(1);
+				}
+				
+				try {
+					InitialWindow.s3Client.deleteBucket(name_to_remove);
+					 System.out.println("bucket removed");
+				} catch (AmazonServiceException e1) {
+				    System.err.println(e1.getErrorMessage());
+				    System.exit(1);
+				}
+				
+				List<Bucket> buckets = InitialWindow.s3Client.listBuckets();
+				model.clear();
+				for (int i = 0; i < buckets.size(); i++) {
+					model.addElement(buckets.get(i).getName());
+				}
+				
+			}
+		});
+		btnDelete.setBounds(102, 228, 82, 23);
+		frame.getContentPane().add(btnDelete);
+
 		List<Bucket> buckets = InitialWindow.s3Client.listBuckets();
+		model.clear();
 		for (int i = 0; i < buckets.size(); i++) {
 			model.addElement(buckets.get(i).getName());
 		}
-		list.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent e) {
-				ListObjectsV2Result result = InitialWindow.s3Client.listObjectsV2(list.getSelectedValue());
-				List<S3ObjectSummary> objects = result.getObjectSummaries();
-				for (int i = 0; i < objects.size(); i++) {
-					model_1.clear();
-					model_1.addElement(objects.get(i).getKey());
-				}
-			}
+//		list.addListSelectionListener(new ListSelectionListener() {
+//			public void valueChanged(ListSelectionEvent e) {
+//				ListObjectsV2Result result = InitialWindow.s3Client.listObjectsV2(list.getSelectedValue());
+//				List<S3ObjectSummary> objects = result.getObjectSummaries();
+//				model_1.clear();
+//				for (int i = 0; i < objects.size(); i++) {
+//					model_1.addElement(objects.get(i).getKey());
+//				}
+//			}
+//
+//		});
 
-		});
-		
-
-		
 	}
 
-//	public void refreshBucketlist() {
-//		model = new DefaultListModel<>();
-//		List<Bucket> buckets = InitialWindow.s3Client.listBuckets();
-//		for (int i = 0; i < buckets.size(); i++) {
-//			model.addElement(buckets.get(i).getName());
-//		}
-//	}
 }
